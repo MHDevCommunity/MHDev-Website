aa98041c738f5fb25c2185d043154b51641808bf
Marc Horne <me@marcushorne.com>
2017-03-08 14:02:17 +0100

    Create url from location vars

--- src/app/services/config.factory.js
+++ src/app/services/config.factory.js
@@ -1,59 +1,61 @@
 angular.module('website').factory('Config', ['$http', '$location', function ConfigFactory($http, $location) {
   'use strict';
+  
+  var url = $location.protocol() + '://' + $location.host() + ':5000';
     
   return {
     
     getArticles: function() {
-      return $http.get('http://localhost:5000/get-articles').then(
+      return $http.get(url + '/get-articles').then(
         function success(data) { return data.data; },
         function error(e) { console.log('Couldn\'t get articles', e); }
       );
     },
     
     getResources: function() {
-      return $http.get('http://localhost:5000/get-resources').then(
+      return $http.get(url + '/get-resources').then(
         function success(data) { return data.data; },
         function error(e) { console.log('Couldn\'t get resources', e); }
       );
     },
     
     registerUser: function(userData) {
-      return $http.post('http://localhost:5000/register-user', userData).then(
+      return $http.post(url + '/register-user', userData).then(
         function success(data) { return data; },
         function error(e) { console.log('Couldn\'t register', e); }
       );
     },
 
     loginUser: function(userData) {
-      return $http.post('http://localhost:5000/login-user', userData).then(
+      return $http.post(url + '/login-user', userData).then(
         function success(data) { return data.data; },
         function error(e) { console.log('Couldn\'t login', e); }
       );
     },
 
     getRepos: function() {
-      return $http.get('http://localhost:5000/get-repos').then(
+      return $http.get(url + '/get-repos').then(
         function success(data) { return data.data; },
         function error(e) { console.log('Couldn\'t get repos', e); }
       );
     },
 
     getRepoHistory: function(repo) {
-      return $http.get('http://localhost:5000/get-repo-history/' + repo).then(
+      return $http.get(url + '/get-repo-history/' + repo).then(
         function success(data) { return data.data; },
         function error(e) { console.log('Couldn\'t get repos', e); }
       );
     },
 
     getOpenIssues: function(repo) {
-      return $http.get('http://localhost:5000/get-open-issues/' + repo).then(
+      return $http.get(url + '/get-open-issues/' + repo).then(
         function success(data) { return data.data; },
         function error(e) { console.log('Couldn\'t get issues', e); }
       );
     },
 
     getReadme: function(repo) {
-      return $http.get('http://localhost:5000/get-readme/' + repo).then(
+      return $http.get(url + '/get-readme/' + repo).then(
         function success(data) { return data.data; },
         function error(e) { console.log('Couldn\'t get README', e); }
       );
